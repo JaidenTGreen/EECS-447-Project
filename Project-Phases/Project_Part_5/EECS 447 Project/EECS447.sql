1054ea82b3fd184591c3f263a164d72740a85504
Jaiden Green <143838602+JaidenTGreen@users.noreply.github.com>
2025-10-26 17:08:48 -0500

    Add user, user specializations, and reserves tables for library system
    Updated last updated date and added table definitions for User, Client, Librarian, and Reserves.

--- Project-Phases/Project_Part_5/EECS 447 Project/EECS447.sql
+++ Project-Phases/Project_Part_5/EECS 447 Project/EECS447.sql
@@ -3,5 +3,42 @@ NAME: EECS 447 PROJECT
 DESCRIPTION: DATABASE SYSTEM FOR LIBRARY MANAGEMENT
 CONTRIBUTORS: JAIDEN GREEN
 DATE: OCTOBER 22, 2025
-LAST UPDATED: OCTOBER 22, 2025
+LAST UPDATED: OCTOBER 26, 2025
 */
+
+--Creates User Entity Table: Jaiden
+CREATE TABLE USER(
+UID CHAR(8) not null primary key,       --Identification Number for Users
+Fname VARCHAR(20) not null,             --First name of the user 
+Lname VARCHAR(20) not null,             --Last name of the user
+Phone VARCHAR(15) not null,             --Phone number of the user 
+Email VARCHAR(50) not null,             --Email address of the user
+HouseNum VARCHAR(10) not null,          --House number of the user
+City VARCHAR(20) not null,              --City of the user
+State VARCHAR(20) not null,             --State of the user
+ZIP CHAR(5) not null,                   --ZIP code of the user
+Street VARCHAR(50) not null,            --Street of the user
+AptNum VARCHAR(10)                      --Apartment number of the user
+);
+
+--CREATES CLIENT SPECIALIZATION TABLE: Jaiden
+CREATE TABLE Client(
+UID CHAR(8) not null PRIMARY KEY,           --Identification number for User
+AcctStanding INT,                           --Stores how many items are loaned(should not be more than 3)
+MemStart DATE,                              --Date of membership start
+FOREIGN KEY (UID) REFERENCES USER(UID)
+);
+
+--CREATES LIBRARIAN SPECIALIZATION TABLE: Jaiden
+CREATE TABLE Librarian(
+UID CHAR(8) not null PRIMARY KEY,               --Identification number for User
+Salary Float(10,2),                             --Salary of the librarian
+FOREIGN KEY (UID) REFERENCES USER(UID)
+);
+
+--CREATES RESERVES RELATINSHIP TABLE: Jaiden
+CREATE TABLE Reserves(
+CopyID CHAR(8) not null PRIMARY KEY,            --Identification number for Items
+UID CHAR(8) not null,                           --Identification number for User
+FOREIGN KEY (CopyID) REFERENCES Items(CopyID)
+);
